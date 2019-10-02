67a76ea139e94892435df559a94a1aa24230d054
xattacker <xattacker.tao@gmail.com>
2019-10-02 09:54:03 +0800

    update class ApnsTBASender

--- AppPushTestForm/ApnsTBASender.cs
+++ AppPushTestForm/ApnsTBASender.cs
@@ -9,10 +9,10 @@ using System.Security.Cryptography;
 using System.Net.Http.Headers;
 using System.Net;
 using PushSharp.Sender;
+using Xattacker.Utility;
 
 namespace AppPush
 {
-    // TokenBasedAuthentication
     class ApnsTBASender
     {
         public async void Push
@@ -32,26 +32,25 @@ namespace AppPush
             var secretKeyFile = Convert.FromBase64String(privateKey);
             var secretKey = CngKey.Import(secretKeyFile, CngKeyBlobFormat.Pkcs8PrivateBlob);
 
-            var expiration = DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            var expirationSeconds = (long)expiration.TotalSeconds;
+            var expirationSeconds = DateTimeUtility.GetCurrentTimeStamp()/1000;
 
             var payload = new Dictionary<string, object>()
-            {
-                { "iss", paras.TeamID },
-                { "iat", expirationSeconds }
-            };
+                            {
+                                { "iss", paras.TeamID },
+                                { "iat", expirationSeconds }
+                            };
             var header = new Dictionary<string, object>()
-            {
-                { "alg", "ES256" },
-                { "kid", paras.KeyID }
-            };
+                            {
+                                { "alg", "ES256" },
+                                { "kid", paras.KeyID }
+                            };
 
             string accessToken = Jose.JWT.Encode(payload, secretKey, JwsAlgorithm.ES256, header);
 
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls |
-                                         SecurityProtocolType.Tls11 |
-                                         SecurityProtocolType.Tls12 |
-                                         SecurityProtocolType.Ssl3;
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls |
+            //                             SecurityProtocolType.Tls11 |
+            //                             SecurityProtocolType.Tls12 |
+            //                             SecurityProtocolType.Ssl3;
 
             //Development server: api.development.push.apple.com:443
             //Production server: api.push.apple.com:443
@@ -62,60 +61,54 @@ namespace AppPush
             var uri = new Uri(string.Format("https://{0}:{1}/3/device/{2}", host, port, deviceTokens[0]));
 
             var payloadData = JObject.FromObject(new
-            {
-                aps = new
-                {
-                    alert = message.Aps.Alert,
-                    badge = message.Aps.Badge,
-                    sound = message.Aps.Sound
-                }
-            });
+                                {
+                                    aps = new
+                                            {
+                                                alert = message.Aps.Alert,
+                                                badge = message.Aps.Badge,
+                                                sound = message.Aps.Sound
+                                            }
+                                });
 
             byte[] data = System.Text.Encoding.UTF8.GetBytes(payloadData.ToString());
 
             var handler = new Http2MessageHandler();
             var httpClient = new HttpClient(handler);
-            httpClient.DefaultRequestHeaders.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.Timeout = new TimeSpan(0, 0, 30); // 30 seconds
+            //httpClient.DefaultRequestHeaders.Clear();
+            //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             var requestMessage = new HttpRequestMessage();
             requestMessage.RequestUri = uri;
-            //requestMessage.Headers.Add("authorization", string.Format("bearer {0}", accessToken));
-            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
-            requestMessage.Headers.Add("apns-id", Guid.NewGuid().ToString());
+            requestMessage.Headers.Add("authorization", string.Format("bearer {0}", accessToken));
+            //requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+            requestMessage.Headers.Add("apns-id",GUID.GenerateGUID());
             requestMessage.Headers.Add("apns-expiration", "0");
             requestMessage.Headers.Add("apns-priority", "10");
             requestMessage.Headers.Add("apns-topic", paras.BundleID);
             requestMessage.Method = HttpMethod.Post;
             requestMessage.Content = new ByteArrayContent(data);
-            requestMessage.Version = new Version(2, 0);
+           // requestMessage.Version = new Version(2, 0);
 
             try
             {
-                var responseMessage = await httpClient.SendAsync(requestMessage);
-
                 bool succeed = false;
                 string response = null;
+                var responseMessage = await httpClient.SendAsync(requestMessage);
 
                 if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    succeed = true;
-
-                    string responseUuid = string.Empty;
                     IEnumerable<string> values;
                     if (responseMessage.Headers.TryGetValues("apns-id", out values))
                     {
-                        responseUuid = values.First();
+                        response = values.First();
+                        succeed = true;
                     }
-
-                    response = responseUuid;
                 }
                 else
                 {
                     var body = await responseMessage.Content.ReadAsStringAsync();
-                    var json = new JObject();
-                    json = JObject.Parse(body);
-
+                    var json = JObject.Parse(body);
                     response = json.Value<string>("reason");
                 }
 
--- AppPushTestForm/IOS_APNSWindow.xaml.cs
+++ AppPushTestForm/IOS_APNSWindow.xaml.cs
@@ -1,22 +1,12 @@
-﻿using System;
+﻿using AppPush.Data;
+using Microsoft.Win32;
+using PushSharp.Sender;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using AppPush.Data;
-using FCM.Json;
-using Microsoft.Win32;
-using PushSharp.Sender;
 using Xattacker.Utility.Json;
 
 namespace AppPush
